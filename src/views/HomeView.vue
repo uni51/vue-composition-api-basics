58269b7d4043944cc80b636680ae055f0ce41b66
Koichi.Shimomura <unieye51@gmail.com>
2022-08-12 17:26:52 +0900

    4-16 Two-Way Data Binding

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,9 +1,16 @@
 <template>
   <div class="home">
+    <h3>{{ counterTitle }}:</h3>
+
     <div>
       <button @click="decreaseCounter" class="btn">-</button>
       <span class="counter">{{ counter }}</span>
       <button @click="increaseCounter" class="btn">+</button>
+    </div>
+
+    <div class="edit">
+      <h4>Edit counter title:</h4>
+      <input v-model="counterTitle" type="text" />
     </div>
   </div>
 </template>
@@ -11,7 +18,8 @@
 <script setup>
 import { ref } from "vue";
 
-const counter = ref(0);
+const counter = ref(0),
+  counterTitle = ref("My Counter");
 
 const increaseCounter = () => {
   counter.value++;
@@ -22,52 +30,6 @@ const decreaseCounter = () => {
 };
 </script>
 
-<!--
-<script>
-import { ref } from "vue";
-
-export default {
-  setup() {
-    const counter = ref(0);
-
-    const increaseCounter = () => {
-      counter.value++;
-    };
-
-    const decreaseCounter = () => {
-      counter.value--;
-    };
-
-    return {
-      counter,
-      increaseCounter,
-      decreaseCounter,
-    };
-  },
-};
-</script>
--->
-
-<!--
-<script>
-export default {
-  data() {
-    return {
-      counter: 0,
-    };
-  },
-  methods: {
-    increaseCounter() {
-      this.counter++;
-    },
-    decreaseCounter() {
-      this.counter--;
-    },
-  },
-};
-</script>
--->
-
 <style>
 .home {
   text-align: center;
@@ -77,5 +39,8 @@ export default {
 .counter {
   font-size: 40px;
   margin: 10px;
+}
+.edit {
+  margin-top: 60px;
 }
 </style>
